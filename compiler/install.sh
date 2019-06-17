d4edc1d51de9d2824ec3f5ecafa7b6587031285e
Kohei Morita <yosupo06@gmail.com>
2019-06-18 04:26:48 +0900

    add compilers

---
+++ compiler/install.sh
@@ -0,0 +1,7 @@
+echo 'Install dlang'
+wget https://github.com/ldc-developers/ldc/releases/download/v1.15.0/ldc2-1.15.0-linux-x86_64.tar.xz
+tar -xf ldc2-1.15.0-linux-x86_64.tar.xz
+
+echo 'Install Rust'
+wget https://static.rust-lang.org/dist/rust-1.35.0-x86_64-unknown-linux-gnu.tar.gz
+tar -xf rust-1.35.0-x86_64-unknown-linux-gnu.tar.gz
