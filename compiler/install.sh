8f19068ec10a4f826ce64a31963875a2592fca46
Kohei Morita <yosupo06@gmail.com>
2019-10-01 18:06:29 +0900

    add pypy3

--- compiler/install.sh
+++ compiler/install.sh
@@ -7,3 +7,7 @@ wget https://static.rust-lang.org/dist/rust-1.35.0-x86_64-unknown-linux-gnu.tar.
 tar -xf rust-1.35.0-x86_64-unknown-linux-gnu.tar.gz
 mkdir rust
 ./rust-1.35.0-x86_64-unknown-linux-gnu/install.sh --prefix=./rust
+
+echo 'Install Python(PyPy)'
+wget https://bitbucket.org/pypy/pypy/downloads/pypy3.6-v7.1.1-linux64.tar.bz2
+tar -xf pypy3.6-v7.1.1-linux64.tar.bz2
