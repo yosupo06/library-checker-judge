5a5f8b4112a978eabb03356eaba92e5c8e88cbe8
Kohei Morita <yosupo06@gmail.com>
2019-10-12 09:36:52 +0000

    add test account

--- local/launch.sh
+++ local/launch.sh
@@ -9,8 +9,12 @@ done
 
 PGPASSWORD=passwd psql -h localhost -U postgres librarychecker < tables.sql
 
-# test account (name: admin / password: password)
+# test account (name: admin / password: password / admin)
 echo "insert into users(name, passhash, admin) values ('admin', '\$2a\$10\$AqftzLHYcaGH2GxUXiGO/OzHnIMJO.PGMrLFqm7mPbpqZlQrIRrq.', true)" \
+| PGPASSWORD=passwd psql -h localhost -U postgres librarychecker
+
+# test account (name: tester / password: password)
+echo "insert into users(name, passhash, admin) values ('tester', '\$2a\$10\$AqftzLHYcaGH2GxUXiGO/OzHnIMJO.PGMrLFqm7mPbpqZlQrIRrq.', true)" \
 | PGPASSWORD=passwd psql -h localhost -U postgres librarychecker
 
 cd ../../library-checker-problems && ./deploy.py
