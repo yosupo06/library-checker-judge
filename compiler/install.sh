bfe0cd090f6ddadbe2ad44a43d05ff9e2524ca0f
Kohei Morita <yosupo06@gmail.com>
2019-10-13 05:25:59 +0000

    small fix

--- compiler/install.sh
+++ compiler/install.sh
@@ -6,4 +6,4 @@ ln -s /opt/ldc2-1.15.0-linux-x86_64/bin/ldc2 /usr/bin/ldc2
 echo 'Install Python(PyPy)'
 wget https://bitbucket.org/pypy/pypy/downloads/pypy3.6-v7.1.1-linux64.tar.bz2
 tar -xf pypy3.6-v7.1.1-linux64.tar.bz2 -C /opt
-ln -s /opt//pypy3.6-v7.1.1-linux64/bin/pypy3 /usr/bin/pypy3
+ln -s /opt/pypy3.6-v7.1.1-linux64/bin/pypy3 /usr/bin/pypy3
