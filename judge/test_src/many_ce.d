16d73e61a4793c4e905eecbe434cfa142244e4da
Kohei Morita <yosupo06@gmail.com>
2019-10-21 13:21:51 +0000

    fix many_ce.d

--- judge/test_src/many_ce.d
+++ judge/test_src/many_ce.d
@@ -1,9 +1,11 @@
 import std.range, std.conv;
 
-void main() {
+immutable string message = "long string".repeat.take(10).join(" ");
+
+void main() {    
     static foreach (ph; 0..10000) {
         static foreach (i; 0..10000) {
-            pragma(msg, "MESSAGE");
+            pragma(msg, message);
         }
     }
 }
