fc70ce0ec2a0a2d75b2f51515b722717b45a4569
Kohei Morita <yosupo06@gmail.com>
2019-11-19 14:02:49 +0000

    merge master

--- judge/executor/executor_core.cpp
+++ judge/executor/executor_core.cpp
@@ -15,16 +15,17 @@ int main(int argc, char* argv[]) {
             v.push_back(argv[i]);
         }
         v.push_back(nullptr);
-        cerr << execvp(argv[2], v.data()) << endl;        
+        if (execvp(argv[2], v.data()) == -1) exit(1);
     } else {
-        high_resolution_clock::time_point begin = high_resolution_clock::now();        
-        wait(&pid);
+        int status;
+        high_resolution_clock::time_point begin = high_resolution_clock::now();
+        wait(&status);
         high_resolution_clock::time_point end = high_resolution_clock::now();
         auto elapsed_time = duration_cast<milliseconds>(end - begin);
         auto milli = (long double)(elapsed_time.count()) / 1000;
         auto f = fopen(argv[1], "w");
         fprintf(f, "%.10Lf\n", milli);
         fclose(f);
+        exit(status);
     }
-    return 0;
 }
