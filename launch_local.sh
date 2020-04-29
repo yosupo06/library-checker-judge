7c9be4cb0bffb353414f71d7f94550b839d445f6
Kohei Morita <yosupo06@gmail.com>
2020-04-29 16:58:53 +0000

    add test account: judge

--- launch_local.sh
+++ launch_local.sh
@@ -14,6 +14,10 @@ PGPASSWORD=passwd psql -h localhost -U postgres librarychecker < tables.sql
 echo "insert into users(name, passhash, admin) values ('admin', '\$2a\$10\$AqftzLHYcaGH2GxUXiGO/OzHnIMJO.PGMrLFqm7mPbpqZlQrIRrq.', true)" \
 | PGPASSWORD=passwd psql -h localhost -U postgres librarychecker
 
+# test account (name: judge / password: password / admin)
+echo "insert into users(name, passhash, admin) values ('judge', '\$2a\$10\$AqftzLHYcaGH2GxUXiGO/OzHnIMJO.PGMrLFqm7mPbpqZlQrIRrq.', true)" \
+| PGPASSWORD=passwd psql -h localhost -U postgres librarychecker
+
 # test account (name: tester / password: password)
 echo "insert into users(name, passhash, admin) values ('tester', '\$2a\$10\$AqftzLHYcaGH2GxUXiGO/OzHnIMJO.PGMrLFqm7mPbpqZlQrIRrq.', false)" \
 | PGPASSWORD=passwd psql -h localhost -U postgres librarychecker
