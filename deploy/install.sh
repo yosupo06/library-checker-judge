7c28d14ce88ae8d9ba7541065ae29853e2dbd7af
Kohei Morita <yosupo06@gmail.com>
2020-07-31 19:21:13 +0900

    rm

--- deploy/install.sh
+++ deploy/install.sh
@@ -18,13 +18,3 @@ dotnet restore /tmp/${project_name} -r ubuntu.18.04-x64
 """ -- library-checker-user
 
 cp -r /tmp/${project_name} ${dirname}/${project_name}
-
-echo 'Install Haskell packages'
-
-stack upgrade --binary-only
-
-su -c """
-stack install --resolver lts-16.3 array bytestring containers deepseq hashable heaps io-streams lens mutable-containers massiv mono-traversable mtl random strict text transformers vector vector-algorithms word8 &&
-cp /var/haskell_load.hs /tmp/haskell_load.hs &&
-cd ~ && stack ghc -- /tmp/haskell_load.hs
-""" -- library-checker-user
