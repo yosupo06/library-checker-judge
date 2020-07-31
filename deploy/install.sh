d8be1d691708ea64b7ff90b347b744e3194ded2e
Kohei Morita <yosupo06@gmail.com>
2020-07-31 11:56:55 +0900

    to apt

--- deploy/install.sh
+++ deploy/install.sh
@@ -31,7 +31,12 @@ cp -r /tmp/${project_name} ${dirname}/${project_name}
 
 echo 'Install Haskell packages'
 
-curl -sSL https://get.haskellstack.org/ | sh
-stack install --resolver lts-16.3 array bytestring containers deepseq hashable heaps io-streams lens mutable-containers massiv mono-traversable mtl random strict text transformers vector vector-algorithms word8
+#curl -sSL https://get.haskellstack.org/ | sh
+#stack install --resolver lts-16.3 array bytestring containers deepseq hashable heaps io-streams lens mutable-containers massiv mono-traversable mtl random strict text transformers vector vector-algorithms word8
+
+su -c """
+cp /var/haskell_load.hs /tmp/haskell_load.hs &&
+cd ~ && stack ghc -- /tmp/haskell_load.hs
+""" -- library-checker-user
 
 exit 0
