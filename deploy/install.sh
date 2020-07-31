f0718f97311d12a38b7f60e3aaca7fd5c83710a3
Kohei Morita <yosupo06@gmail.com>
2020-07-31 19:18:13 +0900

    upgrade version

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,13 +1,3 @@
-echo 'Install dlang'
-wget https://github.com/ldc-developers/ldc/releases/download/v1.15.0/ldc2-1.15.0-linux-x86_64.tar.xz
-tar -xf ldc2-1.15.0-linux-x86_64.tar.xz -C /opt
-ln -s /opt/ldc2-1.15.0-linux-x86_64/bin/ldc2 /usr/bin/ldc2
-
-echo 'Install Python(PyPy)'
-wget https://bitbucket.org/pypy/pypy/downloads/pypy3.6-v7.1.1-linux64.tar.bz2
-tar -xf pypy3.6-v7.1.1-linux64.tar.bz2 -C /opt
-ln -s /opt/pypy3.6-v7.1.1-linux64/bin/pypy3 /usr/bin/pypy3
-
 echo 'Install .NET Core'
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 apt install -y  ./packages-microsoft-prod.deb
