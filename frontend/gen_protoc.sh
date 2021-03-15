d40815d23a3bccbe4fb4bdc405afe81bbb1473ce
Kohei Morita <yosupo06@gmail.com>
2021-03-15 19:56:33 +0900

    test

--- frontend/gen_protoc.sh
+++ frontend/gen_protoc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 pdir=$(cd $(dirname $0)/..;pwd)
 echo $pdir
+ls $pdir
 docker run -v $pdir:/defs namely/protoc-all -i library-checker-judge/api/proto -f library_checker.proto -l web -o library-checker-frontend/src/api
