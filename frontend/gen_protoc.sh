4da1aa772607797a0efacaeda52016801ef0a069
Kohei Morita <yosupo06@gmail.com>
2021-03-15 19:47:33 +0900

    fix

--- frontend/gen_protoc.sh
+++ frontend/gen_protoc.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-
-echo '@test'
-ls $(pwd)
-
-echo '@test2'
-ls ../library-checker-judge/api/proto
-
-cd .. && docker run -v $(pwd):/defs namely/protoc-all -i library-checker-judge/api/proto -f library_checker.proto -l web -o library-checker-frontend/src/api
+cd .. && docker run -v `pwd`:/defs namely/protoc-all -i library-checker-judge/api/proto -f library_checker.proto -l web -o library-checker-frontend/src/api
