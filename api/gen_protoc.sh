a3ed6fb0202b4e57366a9129ec59a21a6c71a3fb
Kohei Morita <yosupo06@gmail.com>
2021-09-08 02:10:15 +0900

    update protoc version

--- api/gen_protoc.sh
+++ api/gen_protoc.sh
@@ -3,4 +3,4 @@
 set -e -o pipefail
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-docker run -v $SCRIPT_DIR:/defs namely/protoc-all:1.34_4 -f proto/library_checker.proto -l go -o .
+docker run -v $SCRIPT_DIR:/defs namely/protoc-all:1.37_2 -l go -f proto/library_checker.proto -o . --go-module-prefix github.com/yosupo06/library-checker-judge/api
