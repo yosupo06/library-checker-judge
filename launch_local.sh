f61a0621bd9583f5af5e85f5764f22e860f75c1c
Kohei Morita <yosupo06@gmail.com>
2021-09-19 01:34:18 +0900

    move sql init

--- launch_local.sh
+++ launch_local.sh
@@ -13,24 +13,6 @@ until PGPASSWORD=passwd psql -c 'select 1;' -U postgres -h localhost 2>&1 > /dev
     sleep 1
 done
 
-PGPASSWORD=passwd psql -h localhost -U postgres librarychecker < tables.sql
-
-# test account (name: admin / password: password / admin)
-echo "insert into users(name, passhash, admin) values ('admin', '\$2a\$10\$AqftzLHYcaGH2GxUXiGO/OzHnIMJO.PGMrLFqm7mPbpqZlQrIRrq.', true)" \
-| PGPASSWORD=passwd psql -h localhost -U postgres librarychecker
-
-# test account (name: judge / password: password / admin)
-echo "insert into users(name, passhash, admin) values ('judge', '\$2a\$10\$AqftzLHYcaGH2GxUXiGO/OzHnIMJO.PGMrLFqm7mPbpqZlQrIRrq.', true)" \
-| PGPASSWORD=passwd psql -h localhost -U postgres librarychecker
-
-# test account (name: upload / password: password / admin)
-echo "insert into users(name, passhash, admin) values ('upload', '\$2a\$10\$AqftzLHYcaGH2GxUXiGO/OzHnIMJO.PGMrLFqm7mPbpqZlQrIRrq.', true)" \
-| PGPASSWORD=passwd psql -h localhost -U postgres librarychecker
-
-# test account (name: tester / password: password)
-echo "insert into users(name, passhash, admin) values ('tester', '\$2a\$10\$AqftzLHYcaGH2GxUXiGO/OzHnIMJO.PGMrLFqm7mPbpqZlQrIRrq.', false)" \
-| PGPASSWORD=passwd psql -h localhost -U postgres librarychecker
-
 sleep 5 # wait to launch
 
 cd deploy && ./gen_protoc.sh && cd ..
---
+++ tools/db_init/1_tables.sql
@@ -0,0 +1,50 @@
+create table users (  
+  name varchar(255) primary key,
+  passhash varchar(255),
+  admin boolean,
+  email varchar(255),
+  library_url varchar(255)
+);
+
+create table problems (
+  name varchar(255) primary key,
+  title varchar(255),
+  source_url varchar(255),
+  statement text,
+  timelimit int,
+  testhash varchar(255)
+);
+
+create table submissions (
+  id serial primary key,
+  submit_time timestamp with time zone,
+  user_name varchar(255) references users(name),
+  problem_name varchar(255) not null references problems(name),
+  lang varchar(32) not null,
+  source text not null,
+  status varchar(32), -- AC, WA, TLE, WJ, ...
+  prev_status varchar(32),
+  hacked boolean,
+  testhash varchar(255),
+  compile_error text,
+  max_time int,
+  max_memory bigint,
+  judge_ping timestamp with time zone,
+  judge_name varchar(255) not null
+);
+
+create table tasks ( -- Between front and judge
+  id serial primary key,
+  priority int not null,
+  available timestamp with time zone,
+  submission int not null references submissions(id)
+);
+
+create table submission_testcase_results (
+  submission int references submissions(id),       -- primary main
+  testcase varchar(63), -- primary sub
+  status varchar(32),
+  time int,
+  memory bigint,
+  primary key(submission, testcase)
+);
