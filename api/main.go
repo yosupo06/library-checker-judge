56dfb47dd37ca4584112c6ffdd62b43fa889bfab
Kohei Morita <yosupo06@gmail.com>
2022-06-03 20:50:35 +0900

    set port by env

--- api/main.go
+++ api/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	pb "github.com/yosupo06/library-checker-judge/api/proto"
@@ -61,7 +61,12 @@ func NewGRPCServer(db *gorm.DB, langsTomlPath string) *grpc.Server {
 func main() {
 	langsTomlPath := flag.String("langs", "../langs/langs.toml", "toml path of langs.toml")
 	isGRPCWeb := flag.Bool("grpcweb", false, "launch gRPCWeb server")
-	port := flag.Int("port", 50051, "port number")
+	port := getEnv("PORT", "50051")
+	portArg := flag.Int("port", -1, "port number")
+
+	if *portArg != -1 {
+		port = strconv.Itoa(*portArg)
+	}
 
 	flag.Parse()
 
@@ -79,7 +84,7 @@ func main() {
 	if *isGRPCWeb {
 		log.Print("launch gRPCWeb server port=", port)
 		wrappedGrpc := grpcweb.WrapServer(s)
-		http.ListenAndServe(fmt.Sprintf(":%d", *port), http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		http.ListenAndServe(":"+port, http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
 			resp.Header().Set("Access-Control-Allow-Origin", "*")
 			resp.Header().Set("Access-Control-Allow-Headers", "Content-Type, x-user-agent, x-grpc-web, authorization")
 			if wrappedGrpc.IsGrpcWebRequest(req) {
@@ -88,7 +93,7 @@ func main() {
 		}))
 	} else {
 		log.Print("launch gRPC server port=", port)
-		listen, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+		listen, err := net.Listen("tcp", ":"+port)
 		if err != nil {
 			log.Fatal(err)
 		}
