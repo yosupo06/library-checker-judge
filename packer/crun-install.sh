8dbcbb1cd70697e472207f42bcb1348eb00f1279
Kohei Morita <yosupo06@gmail.com>
2022-06-26 07:45:58 +0900

    stop to remove docker files

--- packer/crun-install.sh
+++ packer/crun-install.sh
@@ -2,14 +2,5 @@
 
 set -e
 
-apt-get install -y make git gcc build-essential pkgconf libtool \
-   libsystemd-dev libprotobuf-c-dev libcap-dev libseccomp-dev libyajl-dev \
-   go-md2man autoconf python3 automake
-
-cd /root/
-git clone https://github.com/containers/crun -b 1.4.5
-cd /root/crun
-
-./autogen.sh
-./configure
-make
+wget https://github.com/containers/crun/releases/download/1.4.5/crun-1.4.5-linux-amd64 -O /root/crun
+chmod +x /root/crun
