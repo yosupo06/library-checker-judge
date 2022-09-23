d4fef90656c5ec54b8d469e79aa06b5d4c30e9c4
Kohei Morita <yosupo06@gmail.com>
2022-09-24 04:06:51 +0900

    update

--- deploy/gen_protoc.sh
+++ deploy/gen_protoc.sh
@@ -3,5 +3,5 @@
 set -e -o pipefail
 
 cp ../api/proto/library_checker.proto .
-docker run -v `pwd`:/defs namely/protoc-all:1.37_2 -f library_checker.proto -l python -o .
+docker run -v `pwd`:/defs namely/protoc-all:1.49_0 -f library_checker.proto -l python -o .
 rm library_checker.proto
