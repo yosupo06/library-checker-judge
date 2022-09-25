a6f282e2c0d9fc694b7f416548f8616d88f7479c
Kohei Morita <yosupo06@gmail.com>
2022-09-25 19:02:37 +0900

    refactor problems_deploy.py

--- deploy/problems_deploy.py
+++ deploy/problems_deploy.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
-import hashlib
 import tempfile
 import zipfile
 from logging import Logger, basicConfig, getLogger
-from os import environ, getenv, path
+from os import getenv
 from pathlib import Path
 
 import colorlog
@@ -17,7 +16,7 @@ from problem import Problem, find_problem_dir
 import library_checker_pb2 as libpb
 import library_checker_pb2_grpc
 
-from typing import List
+from typing import List, Optional
 
 logger: Logger = getLogger(__name__)
 
@@ -30,6 +29,121 @@ def deploy_categories(stub: library_checker_pb2_grpc.LibraryCheckerServiceStub, 
             problems=x['problems']
         ), categories['categories'])
     ), credentials=cred_token)
+
+
+def find_info_tomls(rootdir: Path, problems: List[str]) -> List[Path]:
+    if len(problems) == 0:
+        # enumerate all problems
+        return list(filter(lambda p: not p.match(
+            'test/**/info.toml'), rootdir.glob('**/info.toml')))
+
+    tomls: List[Path] = []
+    for problem_name in problems:
+        problem_dir = find_problem_dir(rootdir, problem_name)
+        if problem_dir is None:
+            raise ValueError('Cannot find problem: {}'.format(problem_name))
+        tomls.append(problem_dir / 'info.toml')
+    return tomls
+
+
+def connect_api_server(host: str, use_ssl: bool) -> library_checker_pb2_grpc.LibraryCheckerServiceStub:
+    logger.info('connect to API server, host={} ssl={}'.format(host, use_ssl))
+    if use_ssl:
+        channel = grpc.secure_channel(
+            host, grpc.ssl_channel_credentials())
+        return library_checker_pb2_grpc.LibraryCheckerServiceStub(channel)
+    else:
+        channel = grpc.secure_channel(
+            host, grpc.local_channel_credentials())
+        return library_checker_pb2_grpc.LibraryCheckerServiceStub(channel)
+
+
+def get_upload_token(stub: library_checker_pb2_grpc.LibraryCheckerServiceStub, password: str) -> grpc.CallCredentials:
+    response = stub.Login(libpb.LoginRequest(name='upload', password=password))
+    return grpc.access_token_call_credentials(response.token)
+
+
+def get_server_problem_version(stub: library_checker_pb2_grpc.LibraryCheckerServiceStub, cred_token: grpc.CallCredentials, problem_name: str) -> Optional[str]:
+    try:
+        return stub.ProblemInfo(libpb.ProblemInfoRequest(
+            name=problem_name), credentials=cred_token).case_version
+    except grpc.RpcError as err:
+        if err.code() == grpc.StatusCode.UNKNOWN:
+            return None
+        else:
+            raise RuntimeError('unknown gRPC error:', err)
+
+
+def sort_by_deploy_order(info_tomls: List[Path], stub: library_checker_pb2_grpc.LibraryCheckerServiceStub, cred_token: grpc.CallCredentials) -> List[Path]:
+    tomls_new: List[Path] = []
+    tomls_old: List[Path] = []
+
+    for toml_path in info_tomls:
+        probdir = toml_path.parent
+        name = probdir.name
+        problem = Problem(rootdir, probdir)
+
+        new_version = problem.testcase_version()
+        old_version = get_server_problem_version(stub, cred_token, name)
+
+        if old_version is None or new_version != old_version:
+            tomls_new.append(toml_path)
+        else:
+            tomls_old.append(toml_path)
+
+    logger.info('prioritized problems: {}'.format(tomls_new))
+
+    return tomls_new + tomls_old
+
+
+def deploy_probelms(stub: library_checker_pb2_grpc.LibraryCheckerServiceStub, cred_token: grpc.CallCredentials, rootdir: Path, info_tomls: List[Path]):
+    info_tomls = sort_by_deploy_order(info_tomls, stub, cred_token)
+
+    for toml_path in info_tomls:
+        probdir = toml_path.parent
+        name = probdir.name
+        problem = Problem(rootdir, probdir)
+
+        new_version = problem.testcase_version()
+        old_version = get_server_problem_version(stub, cred_token, name)
+
+        logger.info('generate : {} (version {} -> {})'.format(name,
+                                                              old_version, new_version))
+
+        problem.generate(problem.Mode.DEFAULT, None)
+
+        title = problem.config['title']
+        source_url = "https://github.com/yosupo06/library-checker-problems/tree/master/{}/{}".format(
+            probdir.parent.name,
+            probdir.name
+        )
+        timelimit = problem.config['timelimit']
+
+        if new_version != old_version:
+            with tempfile.NamedTemporaryFile(suffix='.zip', delete=False) as tmp:
+                with zipfile.ZipFile(tmp.name, 'w', zipfile.ZIP_DEFLATED) as newzip:
+                    def zip_write(filename, arcname):
+                        newzip.write(filename, arcname)
+                    zip_write(probdir / 'checker.cpp', arcname='checker.cpp')
+                    for f in sorted(probdir.glob('in/*.in')):
+                        zip_write(f, arcname=f.relative_to(probdir))
+                    for f in sorted(probdir.glob('out/*.out')):
+                        zip_write(f, arcname=f.relative_to(probdir))
+
+                minio_client.fput_object(
+                    bucket_name, new_version + '.zip', tmp.name, part_size=5 * 1000 * 1000 * 1000)
+
+                # TODO: stop to remove old file manually, use auto delete
+                if old_version is not None:
+                    minio_client.remove_object(
+                        bucket_name, old_version + '.zip')
+
+        html = problem.gen_html()
+        statement = html.statement
+        stub.ChangeProblemInfo(libpb.ChangeProblemInfoRequest(
+            name=name, title=title, statement=statement, time_limit=timelimit, case_version=new_version, source_url=source_url
+        ), credentials=cred_token)
+
 
 if __name__ == "__main__":
     handler = colorlog.StreamHandler()
@@ -69,32 +183,13 @@ if __name__ == "__main__":
                         help='Bucket name of minio')
 
     args = parser.parse_args()
+
     rootdir = args.root
-    tomls: List[Path] = []
-    for problem_name in args.problem:
-        problem_dir = find_problem_dir(rootdir, problem_name)
-        if problem_dir is None:
-            logger.error('Cannot find problem: {}'.format(problem_name))
-            raise ValueError('Cannot find problem: {}'.format(problem_name))
-        tomls.append(problem_dir / 'info.toml')
-    if len(tomls) == 0:
-        tomls = list(filter(lambda p: not p.match(
-            'test/**/info.toml'), rootdir.glob('**/info.toml')))
 
-    logger.info('connect to API {} ssl={}'.format(args.host, args.prod))
-    if args.prod:
-        channel = grpc.secure_channel(
-            args.host, grpc.ssl_channel_credentials())
-        stub = library_checker_pb2_grpc.LibraryCheckerServiceStub(channel)
-    else:
-        channel = grpc.secure_channel(
-            args.host, grpc.local_channel_credentials())
-        stub = library_checker_pb2_grpc.LibraryCheckerServiceStub(channel)
+    tomls = find_info_tomls(rootdir, args.problem)
 
-    api_password = args.api_pass
-    response = stub.Login(libpb.LoginRequest(
-        name='upload', password=api_password))
-    cred_token = grpc.access_token_call_credentials(response.token)
+    stub = connect_api_server(args.host, args.prod)
+    cred_token = get_upload_token(stub, args.api_pass)
 
     logger.info('connect to ObjectStorage')
     minio_client = Minio(args.minio_host,
@@ -108,84 +203,4 @@ if __name__ == "__main__":
         minio_client.make_bucket(bucket_name)
 
     deploy_categories(stub, cred_token, rootdir)
-    tomls_new: List[Path] = []
-    tomls_old: List[Path] = []
-
-    for toml_path in tomls:
-        probdir = toml_path.parent
-        name = probdir.name
-        problem = Problem(rootdir, probdir)
-
-        new_version = problem.testcase_version()
-        first_time = "FirstTime"
-
-        try:
-            old_version = stub.ProblemInfo(libpb.ProblemInfoRequest(
-                name=name), credentials=cred_token).case_version
-        except grpc.RpcError as err:
-            if err.code() == grpc.StatusCode.UNKNOWN:
-                old_version = first_time
-            else:
-                raise RuntimeError('Unknown gRPC error')
-
-        if new_version != old_version:
-            tomls_new.append(toml_path)
-        else:
-            tomls_old.append(toml_path)
-
-    logger.info('First deploy: {}'.format(tomls_new))
-    logger.info('Second deploy: {}'.format(tomls_old))
-
-    tomls = tomls_new + tomls_old
-
-    for toml_path in tomls:
-        probdir = toml_path.parent
-        name = probdir.name
-        problem = Problem(rootdir, probdir)
-
-        new_version = problem.testcase_version()
-        first_time = "FirstTime"
-
-        try:
-            old_version = stub.ProblemInfo(libpb.ProblemInfoRequest(
-                name=name), credentials=cred_token).case_version
-        except grpc.RpcError as err:
-            if err.code() == grpc.StatusCode.UNKNOWN:
-                old_version = first_time
-            else:
-                raise RuntimeError('Unknown gRPC error')
-
-        logger.info('Generate : {} ({} -> {})'.format(name,
-                                                      old_version, new_version))
-
-        problem.generate(problem.Mode.DEFAULT, None)
-
-        title = problem.config['title']
-        source_url = "https://github.com/yosupo06/library-checker-problems/tree/master/{}/{}".format(
-            probdir.parent.name,
-            probdir.name
-        )
-        timelimit = problem.config['timelimit']
-
-        if new_version != old_version:
-            with tempfile.NamedTemporaryFile(suffix='.zip', delete=False) as tmp:
-                with zipfile.ZipFile(tmp.name, 'w', zipfile.ZIP_DEFLATED) as newzip:
-                    def zip_write(filename, arcname):
-                        newzip.write(filename, arcname)
-                    zip_write(probdir / 'checker.cpp', arcname='checker.cpp')
-                    for f in sorted(probdir.glob('in/*.in')):
-                        zip_write(f, arcname=f.relative_to(probdir))
-                    for f in sorted(probdir.glob('out/*.out')):
-                        zip_write(f, arcname=f.relative_to(probdir))
-
-                minio_client.fput_object(
-                    bucket_name, new_version + '.zip', tmp.name, part_size=5 * 1000 * 1000 * 1000)
-                if old_version != first_time:
-                    minio_client.remove_object(
-                        bucket_name, old_version + '.zip')
-
-        html = problem.gen_html()
-        statement = html.statement
-        stub.ChangeProblemInfo(libpb.ChangeProblemInfoRequest(
-            name=name, title=title, statement=statement, time_limit=timelimit, case_version=new_version, source_url=source_url
-        ), credentials=cred_token)
+    deploy_probelms(stub, cred_token, rootdir, tomls)
