855ea5af4dc2b0e68a811f3f6f11cb8b9a390de7
Kohei Morita <yosupo06@gmail.com>
2023-01-25 04:58:43 +0900

    fix script

--- packer/base/prepare-docker.sh
+++ packer/base/prepare-docker.sh
@@ -7,6 +7,6 @@ mount -t tmpfs -o size=13g /dev/shm /var/lib/docker
 
 rsync -aXS /var/lib/docker-base/. /var/lib/docker
 
-mkdir /sys/fs/cgroup/judge-docker.slice/
-echo '0,1' > /sys/fs/cgroup/test.slice/cpuset.cpus
-echo 'root' > /sys/fs/cgroup/test.slice/cpuset.cpus.partition
+mkdir /sys/fs/cgroup/judge.slice/
+echo '0,1' > /sys/fs/cgroup/judge.slice/cpuset.cpus
+echo 'root' > /sys/fs/cgroup/judge.slice/cpuset.cpus.partition
