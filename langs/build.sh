e4c6368542dca5d95280c3944c190d0f08433f3b
Kohei Morita <yosupo06@gmail.com>
2023-03-20 19:48:32 +0900

    rm unused

--- langs/build.sh
+++ langs/build.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 docker build -t library-checker-images-gcc -f $SCRIPT_DIR/Dockerfile.GCC $SCRIPT_DIR
 docker build -t library-checker-images-ldc -f $SCRIPT_DIR/Dockerfile.LDC $SCRIPT_DIR
 docker build -t library-checker-images-python3 -f $SCRIPT_DIR/Dockerfile.PYTHON3 $SCRIPT_DIR
-docker build -t library-checker-images-haskell -f $SCRIPT_DIR/Dockerfile.HASKELL $SCRIPT_DIR --compress
+docker build -t library-checker-images-haskell -f $SCRIPT_DIR/Dockerfile.HASKELL $SCRIPT_DIR
 docker build -t library-checker-images-csharp -f $SCRIPT_DIR/Dockerfile.CSHARP $SCRIPT_DIR
 docker build -t library-checker-images-rust -f $SCRIPT_DIR/Dockerfile.RUST $SCRIPT_DIR
 docker build -t library-checker-images-java -f $SCRIPT_DIR/Dockerfile.JAVA $SCRIPT_DIR
