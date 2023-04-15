642e05054ff40e614d0f9e2275ddc03b5d7a343d
Kohei Morita <yosupo06@gmail.com>
2023-04-15 18:25:05 +0900

    enforce order of judge

--- database/task_test.go
+++ database/task_test.go
@@ -58,3 +58,32 @@ func TestTask(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestTaskSamePriority(t *testing.T) {
+	db := createTestDB(t)
+
+	if err := PushTask(db, 123, 10); err != nil {
+		t.Fatal(err)
+	}
+	if err := PushTask(db, 124, 10); err != nil {
+		t.Fatal(err)
+	}
+	if err := PushTask(db, 125, 10); err != nil {
+		t.Fatal(err)
+	}
+
+	task1, err := PopTask(db, "judge")
+	if task1 == nil || task1.Submission != 123 || err != nil {
+		t.Fatal(task1, err)
+	}
+
+	task2, err := PopTask(db, "judge")
+	if task2 == nil || task2.Submission != 124 || err != nil {
+		t.Fatal(task2, err)
+	}
+
+	task3, err := PopTask(db, "judge")
+	if task3 == nil || task3.Submission != 125 || err != nil {
+		t.Fatal(task3, err)
+	}
+}
