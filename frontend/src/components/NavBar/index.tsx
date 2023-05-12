1cc88928c000a017516acbe391c55b0451a72da2
hotmanww <hotmanww@gmail.com>
2023-05-12 17:46:15 +0900

    support responsive

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -5,6 +5,7 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Menu from "@mui/material/Menu";
 import { GitHub } from "@mui/icons-material";
+import MenuIcon from "@mui/icons-material/Menu";
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
@@ -13,7 +14,7 @@ import flagJA from "./flag_ja.svg";
 import flagUS from "./flag_us.svg";
 import { styled } from "@mui/system";
 import { useUserInfo } from "../../api/client_wrapper";
-import { Box } from "@mui/material";
+import { Drawer, IconButton } from "@mui/material";
 
 const NavbarLink = styled(Link)({
   color: "inherit",
@@ -41,7 +42,7 @@ const LangSelect = () => {
       }
       sx={{
         ml: "auto",
-        mr: 0.2,
+        mr: { xs: "auto", sm: 0.2 },
       }}
     >
       <MenuItem value="en">
@@ -66,7 +67,7 @@ const ToolsMenu: React.FC = () => {
   };
 
   return (
-    <Box>
+    <>
       <NavbarButton color="inherit" onClick={handleClick}>
         Tools
       </NavbarButton>
@@ -87,7 +88,7 @@ const ToolsMenu: React.FC = () => {
           <NavbarLink to={`/tool/statementviewer`}>Statement Viewer</NavbarLink>
         </MenuItem>
       </Menu>
-    </Box>
+    </>
   );
 };
 
@@ -156,42 +157,88 @@ const NavBar: React.FC = () => {
   const isDeveloper =
     userInfoQuery.isSuccess && userInfoQuery.data.user?.isDeveloper;
   const userMenu = UserMenu();
+  const elements = (
+    <>
+      <Button color="inherit" href="/submissions">
+        Submissions
+      </Button>
+      <Button color="inherit" href="/ranking">
+        Ranking
+      </Button>
+      <Button color="inherit" href="/help">
+        Help
+      </Button>
+      {isDeveloper && <ToolsMenu />}
+      <LangSelect />
+      {userMenu}
+      <Button
+        color="inherit"
+        href="https://github.com/yosupo06/library-checker-problems"
+        target="_blank"
+        rel="noopener"
+      >
+        <GitHub />
+      </Button>
+    </>
+  );
+
+  const title = (
+    <>
+      <Button
+        color="inherit"
+        href="/"
+        sx={{
+          fontSize: "16px",
+          fontWeight: "bolder",
+        }}
+      >
+        Library Checker
+      </Button>
+    </>
+  );
+
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawerOpen = () => {
+    setOpen(!open);
+  };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Button
-          color="inherit"
-          href="/"
-          sx={{
-            fontSize: "16px",
-            fontWeight: "bolder",
-          }}
-        >
-          Library Checker
-        </Button>
-        <Button color="inherit" href="/submissions">
-          Submissions
-        </Button>
-        <Button color="inherit" href="/ranking">
-          Ranking
-        </Button>
-        <Button color="inherit" href="/help">
-          Help
-        </Button>
-        {isDeveloper && <ToolsMenu />}
-        <LangSelect />
-        {userMenu}
-        <Button
-          color="inherit"
-          href="https://github.com/yosupo06/library-checker-problems"
-          target="_blank"
-          rel="noopener"
-        >
-          <GitHub />
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar sx={{ display: { xs: "none", sm: "flex" } }}>
+          {title}
+          {elements}
+        </Toolbar>
+
+        <Toolbar sx={{ display: { xs: "flex", sm: "none" } }}>
+          {title}
+          <IconButton
+            onClick={toggleDrawerOpen}
+            sx={{
+              ml: "auto",
+              mr: 0.2,
+            }}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          display: { xs: "flex", sm: "none" },
+        }}
+        PaperProps={{
+          sx: { width: "35%" },
+        }}
+        anchor="left"
+        open={open}
+        onClose={toggleDrawerOpen}
+      >
+        {elements}
+      </Drawer>
+    </>
   );
 };
 
