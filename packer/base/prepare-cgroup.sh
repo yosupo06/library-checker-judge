f72443d916f62688ed24faee973d9193153661cb
Kohei Morita <yosupo06@gmail.com>
2023-11-14 00:16:17 +0900

    try to set cpuset multiple times

--- packer/base/prepare-cgroup.sh
+++ packer/base/prepare-cgroup.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-# we have to launch docker once and re-set cpusets
-# ref: https://github.com/yosupo06/library-checker-judge/issues/346
-docker run --cgroup-parent judge.slice hello-world && true
-echo '0,1' > /sys/fs/cgroup/judge.slice/cpuset.cpus
+
+while true; do
+  # we have to launch docker once and re-set cpusets
+  # ref: https://github.com/yosupo06/library-checker-judge/issues/346
+  docker run --cgroup-parent judge.slice hello-world && break
+  echo '0,1' > /sys/fs/cgroup/judge.slice/cpuset.cpus
+  sleep 1
+done
