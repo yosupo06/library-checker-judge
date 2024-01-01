0fa12d4a4736b2e92c9c6ef28dac82c9d4795c5e
Kohei Morita <yosupo06@gmail.com>
2024-01-02 06:18:32 +0900

    upd

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "gh_provider_id" {
 output "db_migrator_sa_email" {
     value = google_service_account.db_migrator.email
 }
+
+output "main_db_connection_name" {
+    value = google_sql_database_instance.main_test.connection_name
+}
