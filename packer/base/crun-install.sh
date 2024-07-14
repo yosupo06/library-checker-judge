d904d40bb3e124fc37afc4dacef850004f9a0c27
Kohei Morita <yosupo06@gmail.com>
2024-07-14 02:22:47 +0000

    update crun

--- packer/base/crun-install.sh
+++ packer/base/crun-install.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-wget https://github.com/containers/crun/releases/download/1.8.7/crun-1.8.7-linux-amd64 -O /root/crun
+wget https://github.com/containers/crun/releases/download/1.15/crun-1.15-linux-amd64 -O /root/crun
 chmod +x /root/crun
