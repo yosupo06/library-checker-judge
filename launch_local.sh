5c704b045e96b413a715295ef1d9fa16d703f0b4
goropikari <goropikari56@gmail.com>
2025-01-28 02:50:31 +0900

    fix path

--- launch_local.sh
+++ launch_local.sh
@@ -9,4 +9,4 @@ docker compose up -d --build --wait
 
 # deploy sample problems
 PROBLEMS_PATH=$(realpath $PROBLEMS_PATH)
-(cd uploader && go run . -dir $PROBLEMS_PATH $PROBLEMS_PATH/sample/aplusb/info.toml $PROBLEMS_PATH/datastructure/unionfind/info.toml)
+(cd uploader && go run . -dir $PROBLEMS_PATH $PROBLEMS_PATH/sample/aplusb/info.toml $PROBLEMS_PATH/data_structure/unionfind/info.toml)
