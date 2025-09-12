df1a40cae419b62a0ca314eb2349c2fc242764ab
Kohei Morita <yosupo06@gmail.com>
2025-09-12 19:10:11 +0000

    fix

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -97,6 +97,19 @@ resource "google_cloud_run_v2_service" "api_rest" {
         name  = "PGHOST"
         value = "/cloudsql/${google_sql_database_instance.main.connection_name}"
       }
+      env {
+        name  = "PGUSER"
+        value = "postgres"
+      }
+      env {
+        name = "PGPASSWORD"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.postgres_password.secret_id
+            version = "latest"
+          }
+        }
+      }
       volume_mounts {
         name       = "cloudsql"
         mount_path = "/cloudsql"
